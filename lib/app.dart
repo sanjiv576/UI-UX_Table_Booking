efaddbd3cfb29fe8013ba617addf304440cc07c6
sanjiv576 <shresthasanjiv576@gmail.com>
2024-01-02 17:15:45 +0545

    apply dark and light theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:table_booking/provider/is_dark_theme.dart';
 import 'package:table_booking/themes/app_theme.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'router/app_routes.dart';
 
-class App extends StatelessWidget {
+class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final IsDarkTheme = ref.watch(isDarkThemeProvider);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: AppThemes.appDarkTheme(),
-      darkTheme: AppThemes.appDarkTheme(),
+      theme: IsDarkTheme ? AppThemes.appDarkTheme() : AppThemes.appLightTheme(),
+      // darkTheme: AppThemes.appDarkTheme(),
       routes: AppRoutes.getAppRoutes(),
       initialRoute: AppRoutes.navigationRoute,
     );
