72f3109b59365d1cedd2a333d936c4ae0668c49c
sanjiv576 <shresthasanjiv576@gmail.com>
2024-01-04 22:25:18 +0545

    add app bar theme data

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -1,27 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:table_booking/themes/widget_themes/text_themes.dart';
+
+import '../constants/color_constant.dart';
+import 'widget_themes/text_themes.dart';
 
 class AppThemes {
   static appLightTheme() {
     return ThemeData(
-        brightness: Brightness.light,
-        useMaterial3: true,
-        fontFamily: 'Roboto',
-        scaffoldBackgroundColor: Colors.white,
-        textTheme: KTextThemes.lightTextThemes(),
-        iconTheme: const IconThemeData(
-          color: Colors.black,
-        ));
+      brightness: Brightness.light,
+      useMaterial3: true,
+      fontFamily: 'Roboto',
+      scaffoldBackgroundColor: Colors.white,
+      textTheme: KTextThemes.lightTextThemes(),
+      iconTheme: const IconThemeData(
+        color: Colors.black,
+      ),
+      appBarTheme: const AppBarTheme(
+        elevation: 4,
+        backgroundColor: KColors.rose900,
+        iconTheme: IconThemeData(
+          color: Colors.white,
+          size: 32,
+        ),
+        titleTextStyle: TextStyle(color: Colors.white, fontSize: 28),
+        shadowColor: Colors.black,
+      ),
+    );
   }
 
   static appDarkTheme() {
     return ThemeData(
-        brightness: Brightness.dark,
-        useMaterial3: true,
-        textTheme: KTextThemes.darkTextTheme(),
-        iconTheme: const IconThemeData(color: Colors.white),
-        fontFamily: 'Roboto',
-        scaffoldBackgroundColor: const Color(0xFF282C3B),
-        colorScheme: const ColorScheme.dark(background: Colors.yellow));
+      brightness: Brightness.dark,
+      useMaterial3: true,
+      textTheme: KTextThemes.darkTextTheme(),
+      iconTheme: const IconThemeData(color: Colors.white),
+      fontFamily: 'Roboto',
+      scaffoldBackgroundColor: KColors.darkBgColor,
+      colorScheme: const ColorScheme.dark(background: Colors.yellow),
+      appBarTheme: const AppBarTheme(
+        elevation: 4,
+        backgroundColor: KColors.rose900,
+        iconTheme: IconThemeData(
+          color: Colors.white,
+          size: 32,
+        ),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 28,
+        ),
+        shadowColor: Colors.black,
+      ),
+    );
   }
 }
