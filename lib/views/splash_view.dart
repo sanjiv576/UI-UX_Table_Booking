9c73da39eface50ad72f875dacae658284bedf54
sanjiv576 <shresthasanjiv576@gmail.com>
2024-01-04 22:26:03 +0545

    add splash view with app logo, version, and developer name

---
+++ lib/views/splash_view.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../router/app_routes.dart';
+
+class SplashView extends ConsumerStatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  ConsumerState<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends ConsumerState<SplashView> {
+  final _verticalGap = const SizedBox(height: 16);
+
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushNamed(context, AppRoutes.firstIntroRoute);
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(flex: 10, child: Image.asset('assets/images/app-logo.png')),
+          const Spacer(),
+          const Text('Version: 1.0.0'),
+          _verticalGap,
+          const Text('Designed and Developed by Sanjiv Shrestha'),
+          _verticalGap,
+        ],
+      )),
+    );
+  }
+}
