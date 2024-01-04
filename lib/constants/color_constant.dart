48ac4f9ec4d634385ec00d54118078ca7e58d5de
sanjiv576 <shresthasanjiv576@gmail.com>
2024-01-04 22:26:49 +0545

    add constant app colors

---
+++ lib/constants/color_constant.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class KColors {
+  KColors._();
+
+  static const Color darkBgColor = Color(0xFF282C3B);
+  static const Color lightBgColor = Colors.white;
+  static const Color rose900 = Color(0xFF881337);
+  static const Color light50 = Color(0xFFF0F9FF);
+  static const Color light100 = Color(0xFFEBF8FE);
+  static const Color light500 = Color(0xFF0EA5E9);
+  static const Color light600 = Color(0xFF0284C7);
+  static const Color light800 = Color(0xFF0C4A6E);
+  static const Color light900 = Color(0xFF075985);
+  static const Color introBgColor = Color(0xFFDD74AA);
+}
