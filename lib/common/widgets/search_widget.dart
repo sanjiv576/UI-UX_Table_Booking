875f6cbeab75f068325057787f477799c2e7d37e
sanjiv576 <shresthasanjiv576@gmail.com>
2024-01-13 21:29:50 +0545

    add search restaurants

---
+++ lib/common/widgets/search_widget.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../provider/is_dark_theme.dart';
+
+class SearchBarWidget extends ConsumerWidget {
+  final TextEditingController controller;
+  final IconData leadingIcon;
+  final String hintText;
+  final VoidCallback onPress;
+
+  const SearchBarWidget({
+    super.key,
+    required this.controller,
+    required this.leadingIcon,
+    required this.hintText,
+    required this.onPress,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8.0),
+          border: Border.all(
+            color: ref.watch(isDarkThemeProvider) ? Colors.white : Colors.black,
+            width: 1,
+          )),
+      child: Row(
+        children: [
+          Icon(
+            leadingIcon,
+            color: Colors.black,
+          ),
+          const SizedBox(width: 8.0),
+          Expanded(
+            child: TextField(
+              controller: controller,
+              style: const TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                hintText: hintText,
+                hintStyle: Theme.of(context)
+                    .textTheme
+                    .titleSmall!
+                    .copyWith(color: Colors.black),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          const SizedBox(width: 8.0),
+          IconButton(
+            onPressed: onPress,
+            icon: const Icon(Icons.clear),
+            color: Colors.black,
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/views/nav_bar/search_view.dart
+++ lib/views/nav_bar/search_view.dart
@@ -1,5 +1,12 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../common/widgets/search_widget.dart';
+import '../../data/restaurant_data.dart';
+import '../../provider/is_dark_theme.dart';
+import '../restaurants/widgets/restaurant_card_widget.dart';
 
 class SearchView extends ConsumerStatefulWidget {
   const SearchView({super.key});
@@ -9,11 +16,103 @@ class SearchView extends ConsumerStatefulWidget {
 }
 
 class _SearchViewState extends ConsumerState<SearchView> {
+  final _searchController = TextEditingController();
+
+  List<dynamic> _filteredRestaurants = [];
+
+  bool _isInitializeSearching = false;
+
+  final _verticalGap = const SizedBox(height: 16);
+
+  @override
+  void initState() {
+    // getProductData();
+    _searchController.addListener(_onSearching);
+    super.initState();
+  }
+
+  void _onSearching() {
+    final searchQuery = _searchController.text.trim().toLowerCase();
+    if (searchQuery.isEmpty) {
+      setState(() {
+        _isInitializeSearching = false;
+        _filteredRestaurants = [];
+      });
+    } else {
+      setState(() {
+        _isInitializeSearching = true;
+
+        _filteredRestaurants = RestaurantData.restaurants
+            .where((product) =>
+                product.name.toLowerCase().contains(searchQuery) ||
+                product.location.toLowerCase().contains(searchQuery))
+            .toList();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Search'),
+        title: const Text(
+          'Search',
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SearchBarWidget(
+                controller: _searchController,
+                leadingIcon: Icons.search,
+                hintText: 'Enter restaurant by name or location',
+                onPress: () {
+                  setState(() {
+                    _isInitializeSearching = false;
+                    _filteredRestaurants = [];
+                    _searchController.clear();
+                  });
+                },
+              ),
+              _verticalGap,
+              Expanded(
+                child: _isInitializeSearching
+                    ? ListView.separated(
+                        itemCount: _filteredRestaurants.length,
+                        itemBuilder: (context, index) {
+                          var singleProduct = _filteredRestaurants[index];
+                          return GestureDetector(
+                            onTap: () {
+                              log('Product name ; ${singleProduct.name}');
+                              // Navigator.pushNamed(
+                              //     context, AppRoutes.singleProductRoute,
+                              //     arguments: singleProduct);
+                            },
+                            child: RestaurantCardWidget(
+                              isDarkTheme: ref.watch(isDarkThemeProvider),
+                              restaurants: RestaurantData.restaurants,
+                              i: index,
+                              verticalGap: _verticalGap,
+                            ),
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) {
+                          return const Divider(
+                            height: 16,
+                            color: Colors.transparent,
+                          );
+                        },
+                      )
+                    : const Center(
+                        child: Text('No data found'),
+                      ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
